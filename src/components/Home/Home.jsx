b752e4a20025ad632236f678a102b29157ce90a4
Uxlewl <Uxlewl0220@gmail.com>
2024-08-22 16:51:29 +0300

    Добавит главную страницу

---
+++ src/components/Home/Home.jsx
@@ -0,0 +1,13 @@
+const Home = () => {
+  return (
+    <>
+      <h1>Home</h1>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Laboriosam
+        deserunt autem sunt!
+      </p>
+    </>
+  );
+};
+
+export default Home;
