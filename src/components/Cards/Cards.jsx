9341357a84615e2d735dc26039b756525c080e7b
Uxlewl <Uxlewl0220@gmail.com>
2024-09-07 01:47:22 +0300

    Внесены правки в компонент

--- src/components/Cards/Cards.jsx
+++ src/components/Cards/Cards.jsx
@@ -3,27 +3,34 @@ import { useEffect, useState } from "react";
 import Alert from "../Alert/Alert";
 import { useProductStore } from "../../store/useProductStore";
 import useFetchItemsStore from "../../store/useFetchItemsStore";
+import { useNavigate } from "react-router";
 
 const Cards = () => {
+  const navigate = useNavigate();
   const recipes = useFetchItemsStore((state) => state.items); // получаем товары из стора
   const fetchItems = useFetchItemsStore((state) => state.fetchItems); // затягиваем товары из бызы даныых
   const addCartItem = useProductStore((state) => state.addCartItem); // получаем из стора функию для добавления товаров в корзину
 
   const [isAlertOpen, setAlertOpen] = useState(false); // стейт для показа и скрытия уведомления
-  const [isItemName, setItemName] = useState(null); // стейт для получения названия товара и передачи его в компонент уведомление 
+  const [isItemName, setItemName] = useState(null); // стейт для получения названия товара и передачи его в компонент уведомление
 
   // функция добавляет товар в корзину и показываем уведомление
   const handleAddItemToCart = (item) => {
     addCartItem(item);
 
-    setItemName(item.title);
-
+    setItemName(item?.name);
+    
     setAlertOpen(true);
   };
 
   useEffect(() => {
     fetchItems();
   }, [fetchItems]);
+
+  const handleCardClick = (id) => {
+    console.log(id);
+    navigate(`/cards/${id}`);
+  };
 
   console.log(recipes);
 
@@ -35,10 +42,11 @@ const Cards = () => {
             key={product?.id}
             id={product?.id}
             image={product?.img}
-            title={product?.title}
+            title={product?.name}
             description={product?.description}
             price={product?.price}
             click={() => handleAddItemToCart(product)}
+            onCardClick={() => handleCardClick(product.id)}
           />
         ))}
 
