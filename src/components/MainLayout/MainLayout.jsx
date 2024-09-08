9dba7f30ba6595086ee9f1da70ac69ad2737fc20
Uxlewl <Uxlewl0220@gmail.com>
2024-09-08 12:54:03 +0300

    Добавлен футер

--- src/components/MainLayout/MainLayout.jsx
+++ src/components/MainLayout/MainLayout.jsx
@@ -1,16 +1,20 @@
 import { Outlet } from "react-router-dom";
 import Header from "../Header/Header";
+import Footer from "../Footer/Footer";
 
 const MainLayout = () => {
   return (
-    <>
+    <div className="flex flex-col min-h-full">
       <Header />
       <div className="w-[90rem] mx-auto flex flex-wrap gap-12">
         <main className="w-full flex flex-wrap gap-12">
           <Outlet />
         </main>
       </div>
-    </>
+      <div className="flex text-center justify-center">
+        <Footer />
+      </div>
+    </div>
   );
 };
 export default MainLayout;
